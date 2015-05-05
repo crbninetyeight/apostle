63c9d7a9ece4875d547b55779713c849f9bd40dd
Christian Byrne <christianbyrne2012@gmail.com>
2015-05-05 17:45:07 -0400

    first stage of test branch

---
+++ tests/array_cast.cpp
@@ -0,0 +1,44 @@
+/*	array_cast.cpp: can arrays be casted to pointers?
+ *
+ *	given a structure that contains
+ *		a. int**
+ *		b. the given dimensions of the array
+ *
+ *	can an array be casted to it?				*/
+
+#include <iostream>
+
+struct	dime2 {
+	int	width, height;
+};
+
+struct	shelf {
+	int*	book;
+	dime2	dimension;
+};
+
+int	main ( int argc, char** argv )
+{
+	dime2	dimension;
+
+	dimension.width		= 25;
+	dimension.height	= 25;	
+
+	int	books[dimension.width][dimension.height];
+
+	for ( int county = 0; county < dimension.height; county++ ) {
+		for ( int countx = 0; countx < dimension.width; countx++ ) {
+			books[countx][county]	= countx + county;
+		}
+	}
+
+	shelf	bookshelf;
+
+	bookshelf.dimension	= dimension;
+	bookshelf.book		= (int *)books;
+
+	/* now we extract the information from bookshelf */
+	std::cout << (int)bookshelf.book[12][12] << '\n';
+
+	return	0;
+}
