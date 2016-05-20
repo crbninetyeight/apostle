67211e79f414ce4ac2a3409f3504eb18ca483376
Christian Byrne <christianbyrne2012@gmail.com>
2016-05-19 20:05:01 -0400

    ticker wip stage

---
+++ src/ticker.hpp
@@ -0,0 +1,11 @@
+#ifndef APO_TICKER_HPP
+#define APO_TICKER_HPP
+
+#include <SDL2/SDL.h>
+#include <SDL_image.h>
+#include <string>
+
+#include "actor.hpp"
+#include "world.hpp"
+
+#endif // APO_TICKER_HPP
